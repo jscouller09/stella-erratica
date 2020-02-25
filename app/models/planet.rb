9de4f879f147047dcec1d29de60a1015c688bc62
Christina Barreti Sigal <CBarrettiSigal@gmail.com>
2020-02-25 10:29:01 -0500

    added planet validations and associations

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,8 @@
 class Planet < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :rate, presence: true
+  belongs_to :user
+  belongs_to :environment
+  has_many_attached :photos
 end
