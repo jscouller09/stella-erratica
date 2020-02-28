a69255f37cc68233bb2426418c12f369c9d0a0f8
James Scouller <jscouller09@gmail.com>
2020-02-28 12:11:40 -0500

    INitial map testing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,16 @@
 import "bootstrap";
+
+// for space map
+import "leaflet"
+import "leaflet/dist/leaflet.css"
+
+var map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -5
+});
+var bounds = [[-26.5,-25], [1021.5,1023]];
+var image = L.imageOverlay('uqm_map_full.png', bounds).addTo(map);
+console.log(map);
+console.log(bounds);
+console.log(image);
+map.fitBounds(bounds);
