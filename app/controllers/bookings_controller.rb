6e5070d0ab6d1d8ed77d34755b55fec7761fa8d8
Antoine Bonifacj <abonifacj63@gmail.com>
2020-02-28 12:08:36 -0500

    finishing booking new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,12 +9,15 @@ class BookingsController < ApplicationController
   # POST /planets/:planet_id/bookings
   def create
     @planet = Planet.find(params[:planet_id])
-    @booking = Booking.new(booking_params)
+    dates = params[:booking][:start].split(" to ")
+    start_d = Date.parse(dates.first)
+    end_d = Date.parse(dates.last)
+    @booking = Booking.new(start: start_d, end: end_d)
     @booking.user = current_user
     @booking.planet = @planet
     authorize @booking
     @booking.duration = @booking.end - @booking.start
-    @booking.cost = @planet.rate * @booking.duration
+    @booking.cost = @planet.rate * @booking.duration + 1000
     if @booking.save
     # TODO make this redirect to the user dashboard
       redirect_to planet_path(@planet)
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,11 @@ import "../plugins/flatpickr";
 import "flatpickr/dist/themes/airbnb.css"
 import { updateTotal } from '../helpers/invoiceupdater';
 
-const datesdata = document.querySelector('.class')
+const datesdata = document.querySelector('#select_btn')
 
 datesdata.addEventListener('click', (event) => {
   event.preventDefault();
-  const dates = document.getElementById('search_starts');
+  const dates = document.getElementById('booking_start');
   const daterange = dates.value;
   console.log(daterange);
   const firstdate = daterange.slice(0, 10);
@@ -16,7 +16,7 @@ datesdata.addEventListener('click', (event) => {
   const end = new Date(`${lastdate}`);
   const duration = (end - start)/86400000 + 1;
   const counter = document.getElementById('counter');
-  counter.value = duration;
+  counter.innerText = duration;
   updateTotal(duration);
   console.log(duration);
 });
